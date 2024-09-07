9603bd4e04c061deef779723d08019dd47d38128
uhammadziyo <DilafruzIminova71@gmail.com>
2024-09-07 10:24:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+
+
+const MyObject = {name: "John", Age: 30, City: "New York",}
+console.log(MyObject.name); // Outputs: John
+console.log(MyObject['Age']); // Outputs: 30
+console.log(MyObject.City); // Outputs: New york
